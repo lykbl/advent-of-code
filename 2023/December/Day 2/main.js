f2c1b1f50de62f9d6227320ab4d50d38744819b9
rkpiii <deslike33@gmail.com>
2023-12-12 00:37:47 +0100

    2023-12-02-p2: finish

--- 2023/December/Day 2/main.js
+++ 2023/December/Day 2/main.js
@@ -24,6 +24,41 @@ function isGamePossible(setsLine, constraints) {
   return true;
 }
 
+function fewestCubesNeeded(setsLine) {
+  let cubesCount = '';
+  let color = '';
+  const fewestCubesNeeded = {
+    'red': 0,
+    'green': 0,
+    'blue': 0,
+  };
+
+  for (let i = 0; i < setsLine.length; i++) {
+    const char = setsLine[i];
+    if (char === ' ') {
+      continue;
+    } else if (!isNaN(parseInt(char))) {
+      cubesCount += char;
+    } else if (char === ',' || char === ';') {
+      fewestCubesNeeded[color] = Math.max(fewestCubesNeeded[color], parseInt(cubesCount));
+      cubesCount = '';
+      color = '';
+    } else {
+      color += char;
+      if (i === setsLine.length - 1) {
+        fewestCubesNeeded[color] = Math.max(fewestCubesNeeded[color], parseInt(cubesCount));
+      }
+    }
+  }
+
+  let power = 1;
+  for (let count in fewestCubesNeeded) {
+    power *= fewestCubesNeeded[count];
+  }
+
+  return power;
+}
+
 const gameLines = fs.readFileSync(isTest ? './test.txt' : './input.txt', 'utf-8').split('\n');
 const constraints = {
   'red': 12,
@@ -42,5 +77,20 @@ for (let gameLine of gameLines) {
   }
 }
 
+const gameStats = [];
+for (let gameLine of gameLines) {
+  const [gameLabel, setsLine] = gameLine.split(': ');
+  const gameId = new RegExp(/Game ([\d]+)/).exec(gameLabel)[1];
+  const fewestCubes = fewestCubesNeeded(setsLine);
+  gameStats.push({
+    gameId,
+    fewestCubes,
+  });
+}
+
+const totalPower = gameStats.reduce((acc, curr) => acc + curr.fewestCubes, 0);
+
 console.log(impossibleGames)
 console.log(result)
+console.log(gameStats)
+console.log(totalPower)
