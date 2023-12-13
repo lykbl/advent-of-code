4082f804d9abae5fd59ab7d01380c17ffd8ba663
rkpiii <deslike33@gmail.com>
2023-12-13 23:44:05 +0100

    2023-12-04-p1 finish

--- 2023/December/Day 4/main.js
+++ 2023/December/Day 4/main.js
@@ -1,3 +1,56 @@
 const args = process.argv.slice(2);
 const isTest = args.includes('--test');
 const fs = require('fs');
+
+const isDigit = (character) => !isNaN(parseInt(character))
+
+const gameStates = [
+  'initial',
+  'luckyNumbers',
+  'tellResults',
+];
+
+const games = [];
+const input = fs.readFileSync(isTest ? './test.txt' : './input.txt', 'utf-8');
+let luckyNumbers, gameResult, numberAcc;
+let gameState = 0;
+
+for (let i = 0; i < input.length; i++) {
+  let character = input[i];
+  if (character === ':') {
+    gameState = 1;
+    luckyNumbers = {};
+    gameResult = 0;
+    numberAcc = '';
+  }
+
+  if (gameState < 1) {
+    continue;
+  }
+
+  if (isDigit(character)) {
+    numberAcc += character;
+  }
+
+  if (character === '|') {
+    gameState = 2;
+  }
+
+  if ((character === ' ' || character === '\n') && numberAcc.length) {
+    if (gameState === 1) {
+      luckyNumbers[numberAcc] = true;
+    } else if (gameState === 2 && luckyNumbers[numberAcc]) {
+      gameResult === 0 ? gameResult = 1 : gameResult *= 2;
+    }
+    numberAcc = '';
+  }
+  if (character === '\n') {
+    numberAcc = '';
+    gameState = 0;
+    luckyNumbers = {};
+    games.push(gameResult);
+  }
+}
+
+console.log(games)
+console.log(games.reduce((acc, curr) => acc + curr, 0));
