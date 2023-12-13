0b0d380f895e49de77fc744fd5dc61e36ec10177
rkpiii <deslike33@gmail.com>
2023-12-13 23:11:44 +0100

    2023-12-03-p2 finish

--- 2023/December/Day 3/main.js
+++ 2023/December/Day 3/main.js
@@ -3,6 +3,8 @@ const isTest = args.includes('--test');
 const fs = require('fs');
 
 const isPartCharacter = (char) => char !== undefined && isNaN(parseInt(char)) && char !== '.' && char !== '\n';
+
+const isGearCharacter = (char) => char === '*';
 function analyzeNumber(input, firstDigitIndex, lineLength) {
   const currentLine = Math.floor(firstDigitIndex / lineLength);
   const lineStart = currentLine * lineLength
@@ -39,9 +41,48 @@ function analyzeNumber(input, firstDigitIndex, lineLength) {
   }
 }
 
+function detectGears(input, firstDigitIndex, lineLength) {
+  const currentLine = Math.floor(firstDigitIndex / lineLength);
+  const lineStart = currentLine * lineLength
+  const lineEnd = lineStart + lineLength;
+  let middleCursor = firstDigitIndex - 1 > lineStart ? firstDigitIndex - 1 : firstDigitIndex;
+  let lastDigitIndex = firstDigitIndex;
+  let topCursor, bottomCursor;
+  let value = '';
+  let gearLocations = [];
+
+  while (middleCursor >= lineStart && middleCursor <= lineEnd) {
+    topCursor = middleCursor - lineLength;
+    bottomCursor = middleCursor + lineLength;
+    for (let cursor of [topCursor, bottomCursor, middleCursor]) {
+      if (isGearCharacter(input[cursor])) {
+        gearLocations.push(cursor);
+      }
+    }
+
+    if (isNaN(parseInt(input[middleCursor]))) {
+      //better way to break out?
+      if (middleCursor > firstDigitIndex) {
+        break;
+      }
+    } else {
+      value += input[middleCursor];
+      lastDigitIndex = middleCursor;
+    }
+
+    middleCursor += 1;
+  }
+
+  return {
+    value: parseInt(value),
+    gearLocations,
+  };
+}
+
 const input = fs.readFileSync(isTest ? './test.txt' : './input.txt', 'utf-8');
 const lineLength = input.indexOf('\n') + 1;
 const partNumbers = [];
+const gearsMap = {};
 for (let i = 0; i < input.length; i++) {
   const char = input[i];
   if (!isNaN(parseInt(char))) {
@@ -49,10 +90,22 @@ for (let i = 0; i < input.length; i++) {
     if (numberAnalysis.isPartNumber) {
       partNumbers.push(numberAnalysis);
     }
+    const gearAnalysis = detectGears(input, i, lineLength);
+    for (let gearLocation of gearAnalysis.gearLocations) {
+      gearsMap[gearLocation] === undefined ? gearsMap[gearLocation] = [gearAnalysis.value] : gearsMap[gearLocation].push(gearAnalysis.value);
+    }
 
     i = numberAnalysis.lastDigitIndex + 1;
   }
 }
 
-console.log(partNumbers.map(partNumber => partNumber.value))
-console.log(partNumbers.reduce((acc, curr) => acc + curr.value, 0));
+console.log(gearsMap)
+let totalGearRatio = 0;
+for (let gearLocation in gearsMap) {
+  if (gearsMap[gearLocation].length === 2) {
+    totalGearRatio += gearsMap[gearLocation][0] * gearsMap[gearLocation][1];
+  }
+}
+console.log(totalGearRatio)
+// console.log(partNumbers.map(partNumber => partNumber.value))
+// console.log(partNumbers.reduce((acc, curr) => acc + curr.value, 0));
