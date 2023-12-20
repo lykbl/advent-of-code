8024e5a597a467212b8dddb359676387eb8094d3
rkpiii <deslike33@gmail.com>
2023-12-20 10:17:29 +0100

    [2023-D11] minor code improvements

--- 2023/D11/main.js
+++ 2023/D11/main.js
@@ -4,7 +4,6 @@ const fs = require('fs');
 const image = fs.readFileSync(isTest ? './test.txt' : './input.txt', 'utf8');
 
 const isGalaxy = (char) => char === '#';
-
 class Galaxy {
   constructor(x, y, label) {
     this.x = x;
@@ -14,6 +13,7 @@ class Galaxy {
 }
 
 const universe = detectGalaxies(image);
+const grandUniverse = detectGalaxies(image);
 function detectGalaxies(skyImage) {
   const lines = skyImage.split('\n');
   const galaxyCoordinates = [];
@@ -32,45 +32,37 @@ function detectGalaxies(skyImage) {
 
   return galaxyCoordinates;
 }
-function expandUniverse(sortedGalaxies, expansionType, expansionFactor = 2) {
-  let expansion = 0;
-  for (let i = 0; i < sortedGalaxies.length - 1; i++) {
-    const currentGalaxy = sortedGalaxies[i];
-    const nextGalaxy = sortedGalaxies[i + 1];
+function expandUniverse(universe, expansionFactor = 2) {
+  for (let type of ['x', 'y']) {
+    let expansion = 0;
+    universe = universe.sort((a, b) => a[type] - b[type]);
+    for (let i = 0; i < universe.length - 1; i++) {
+      const currentGalaxy = universe[i];
+      const nextGalaxy = universe[i + 1];
 
-    const diff = (nextGalaxy[expansionType] + expansion) - currentGalaxy[expansionType] - 1;
-    if (diff >= 1) {
-      expansion += diff * (expansionFactor - 1);
+      const diff = (nextGalaxy[type] + expansion) - currentGalaxy[type] - 1;
+      if (diff >= 1) {
+        expansion += diff * (expansionFactor - 1);
+      }
+      universe[i + 1][type] += expansion;
     }
-    sortedGalaxies[i + 1][expansionType] += expansion;
+
   }
 
-  return sortedGalaxies;
+  return universe;
+}
+function distanceBetweenGalaxies(galaxyA, galaxyB) {
+  return Math.abs(galaxyA.y - galaxyB.y) + Math.abs(galaxyA.x - galaxyB.x)
 }
 function totalUniversePaths(universe) {
-  let totalPath = 0;
-  for (let i = 0; i < universe.length; i++) {
-    const galaxy = universe[i];
-    for (let y = i + 1; y < universe.length; y++) {
-      const nextGalaxy = universe[y];
-      const shortestPath = Math.abs(nextGalaxy.y - galaxy.y) + Math.abs(nextGalaxy.x - galaxy.x);
-      totalPath += Math.abs(shortestPath);
-    }
-  }
-
-  return totalPath;
+  return universe.reduce((totalPairsPath, galaxy, i) =>
+      totalPairsPath +
+      universe.slice(i + 1).reduce((pairPath, nextGalaxy) => pairPath + distanceBetweenGalaxies(galaxy, nextGalaxy), 0),
+  0);
 }
 
-const expansionFactor = 1000000;
-const basicExpandedUniverse = expandUniverse(
-  expandUniverse(JSON.parse(JSON.stringify(universe)).sort((a, b) => a.x - b.x), 'x').sort((a, b) => a.y - b.y),
-  'y',
-);
-const expandedUniverse = expandUniverse(
-  expandUniverse(universe.sort((a, b) => a.x - b.x), 'x', expansionFactor).sort((a, b) => a.y - b.y),
-  'y',
-  expansionFactor
-);
+const basicExpandedUniverse = expandUniverse(universe);
+const expandedUniverse = expandUniverse(grandUniverse, 1000000);
 
 console.log(totalUniversePaths(basicExpandedUniverse))
 console.log(totalUniversePaths(expandedUniverse))
